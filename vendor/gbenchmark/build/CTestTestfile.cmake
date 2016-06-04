37c133843675dc1b58ab9b6183af18fa3e28ddac
cgwu <cgwu0530@gmail.com>
2016-06-04 11:29:00 -0700

    updated

--- vendor/gbenchmark/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/chenggang/Documents/Research/high-performance-lattices/vendor/gbenchmark
-# Build directory: /home/chenggang/Documents/Research/high-performance-lattices/vendor/gbenchmark/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(src)
-subdirs(test)
