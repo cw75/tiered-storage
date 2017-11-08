e3b9d371764a3f782b3a66ddafa5d67c4535617b
Ubuntu <ubuntu@ip-172-31-6-89.us-west-2.compute.internal>
2017-11-08 09:37:21 +0000

    clean up memory tier code

--- kv_store/lww_kvs/kvs_ebs_server.cpp
+++ kv_store/lww_kvs/kvs_ebs_server.cpp
@@ -43,16 +43,6 @@ typedef KV_Store<string, RC_KVS_PairLattice<string>> Database;
 
 typedef consistent_hash_map<worker_node_t,ebs_hasher> ebs_hash_t;
 
-struct pair_hash {
-  template <class T1, class T2>
-    std::size_t operator () (const std::pair<T1,T2> &p) const {
-      auto h1 = std::hash<T1>{}(p.first);
-      auto h2 = std::hash<T2>{}(p.second);
-
-      return h1 ^ h2;
-    }
-};
-
 // an unordered map to represent the gossip we are sending
 typedef unordered_map<string, RC_KVS_PairLattice<string>> gossip_data;
 
@@ -264,50 +254,6 @@ void send_gossip(changeset_address* change_set_addr, SocketCache& cache, string 
     it->second.SerializeToString(&data);
     zmq_util::send_string(data, &cache[it->first]);
   }
-}
-
-// we have this template because we use responsible for with different hash
-// functions depending on whether we are checking local or remote
-// responsibility
-template<typename N, typename H>
-bool responsible(string key, int rep, consistent_hash_map<N,H>& hash_ring, string ip, node_t& sender_node, bool& remove) {
-  // the ip of the node for which we are checking responsibility
-  bool resp = false;
-
-  auto pos = hash_ring.find(key);
-  // this is just a dummy value
-  auto target_pos = hash_ring.begin();
-
-  // iterate once for every value in the replication factor
-  for (int i = 0; i < rep; i++) {
-    // if one of the replicas is the node we care about, we set response to
-    // true; we also store that node in target_pos
-    if (pos->second.id_.compare(ip) == 0) {
-      resp = true;
-      target_pos = pos;
-    }
-
-    if (++pos == hash_ring.end()) {
-      pos = hash_ring.begin();
-    }
-  }
-
-
-  // at the end of the previous loop, pos has the last node responsible for the
-  // key; we only remove that key if we have more nodes than there should be replicas
-  if (resp && (hash_ring.size() > rep)) {
-    remove = true;
-    sender_node = pos->second;
-  } else if (resp && (hash_ring.size() <= rep)) {
-    remove = false;
-    if (++target_pos == hash_ring.end()) {
-      target_pos = hash_ring.begin();
-    }
-
-    sender_node = target_pos->second;
-  }
-
-  return resp;
 }
 
 // ebs worker event loop
