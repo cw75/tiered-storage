eef82037af4999037e98754b5d83cf9c003e7bc9
cw75 <cgwu0530@gmail.com>
2018-01-19 19:58:38 +0000

    fix empty address bug

--- k8s/start.sh
+++ k8s/start.sh
@@ -9,17 +9,17 @@ cd tiered-storage
 IP=`ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}'`
 
 if [ "$1" = "mn" ]; then
-  sh k8s/set_ips.sh $MEM_IPS conf/monitoring/existing_memory_servers.txt
-  sh k8s/set_ips.sh $EBS_IPS conf/monitoring/existing_ebs_servers.txt
-  sh k8s/set_ips.sh $PROXY_IPS conf/monitoring/proxy_address.txt
+  #sh k8s/set_ips.sh $MEM_IPS conf/monitoring/existing_memory_servers.txt
+  #sh k8s/set_ips.sh $EBS_IPS conf/monitoring/existing_ebs_servers.txt
+  #sh k8s/set_ips.sh $PROXY_IPS conf/monitoring/proxy_address.txt
   echo $MGMT_IP > conf/monitoring/management_ip.txt 
 
   ./build/kv_store/lww_kvs/kvs_monitoring
 elif [ "$1" = "p" ]; then
   echo $IP > conf/proxy/proxy_ip.txt
   echo $MON_IP > conf/proxy/monitoring_address.txt
-  sh k8s/set_ips.sh $MEM_IPS conf/proxy/existing_memory_servers.txt
-  sh k8s/set_ips.sh $EBS_IPS conf/proxy/existing_ebs_servers.txt
+  #sh k8s/set_ips.sh $MEM_IPS conf/proxy/existing_memory_servers.txt
+  #sh k8s/set_ips.sh $EBS_IPS conf/proxy/existing_ebs_servers.txt
 
   ./build/kv_store/lww_kvs/kvs_proxy
 else 
