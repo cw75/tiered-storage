80bc23c1f8802eff19629b5ffa111dcfcd6b2745
cw75 <cgwu0530@gmail.com>
2018-03-12 04:48:07 +0000

    revert io thread number

--- kv_store/lww_kvs/kvs_server.cpp
+++ kv_store/lww_kvs/kvs_server.cpp
@@ -266,7 +266,7 @@ void run(unsigned thread_id) {
 
   // prepare the zmq context
   zmq::context_t context(1);
-  zmq_ctx_set(&context, ZMQ_IO_THREADS, 2);
+  //zmq_ctx_set(&context, ZMQ_IO_THREADS, 2);
 
   SocketCache pushers(&context, ZMQ_PUSH);
 
