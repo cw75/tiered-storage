cb3368e40cf19d58096d0d6cafa677658411bec3
cw75 <cgwu0530@gmail.com>
2018-04-26 13:09:41 +0000

    1:5

--- kv_store/include/common.h
+++ kv_store/include/common.h
@@ -623,7 +623,15 @@ void warmup(unordered_map<string, key_info>& placement) {
     placement[key].local_replication_map_[1] = DEFAULT_LOCAL_REPLICATION;
     placement[key].local_replication_map_[2] = DEFAULT_LOCAL_REPLICATION;
   }*/
-  for (unsigned i = 1; i <= 120000; i++) {
+  for (unsigned i = 1; i <= 20000; i++) {
+    // key is 8 bytes
+    string key = string(8 - to_string(i).length(), '0') + to_string(i);
+    placement[key].global_replication_map_[1] = 1;
+    placement[key].global_replication_map_[2] = 0;
+    placement[key].local_replication_map_[1] = 1;
+    placement[key].local_replication_map_[2] = 1;
+  }
+  for (unsigned i = 20001; i <= 120000; i++) {
     // key is 8 bytes
     string key = string(8 - to_string(i).length(), '0') + to_string(i);
     placement[key].global_replication_map_[1] = 0;
