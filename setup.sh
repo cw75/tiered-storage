b388ab5afca9174aaaaecab59061e4b0cdd464c5
Vikram Sreekanti <vsreekanti@gmail.com>
2018-06-14 14:05:47 -0700

    fix setup script; declutter Dockerfile

--- setup.sh
+++ setup.sh
@@ -1,7 +1,39 @@
 #!/bin/bash
 
-# TODO: check clang, cmake, wget, etc.
+echo "This assumes that you are starting from scratch on a machine. It will install the following packages."
+echo "If you have conflicting versions of any of these packages, this script might fail."
+echo "\t*build-essential"
+echo "\t*autoconf"
+echo "\t*automake"
+echo "\t*libtool"
+echo "\t*curl"
+echo "\t*make"
+echo "\t*g++"
+echo "\t*unzip"
+echo "\tpkg-config"
+echo "\t*wget"
+echo "\t*clang-3.9"
+echo "\t*libc++-dev"
+echo "\t*libc++abi-dev"
+echo "\t*cmake-3.9.4"
+echo "\t*protobuf-3.5.1"
+echo "\t*git"
+
 UNAMESTR=`uname`
+
+echo "Installing apt packages..."
+sudo apt-get update > /dev/null 2>&1
+sudo apt-get install -y build-essential autoconf automake libtool curl make g++ unzip pkg-config wget clang-3.9
+sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-3.9 1
+sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-3.9 1
+sudo apt-get install -y libc++-dev libc++abi-dev awscli jq
+
+echo "Installing cmake..."
+wget https://cmake.org/files/v3.9/cmake-3.9.4-Linux-x86_64.tar.gz
+tar xvzf cmake-3.9.4-Linux-x86_64.tar.gz
+sudo mv cmake-3.9.4-Linux-x86_64 /usr/bin/cmake
+export PATH=$PATH:/usr/bin/cmake/bin
+rm cmake-3.9.4-Linux-x86_64.tar.gz
 
 # Check if protobuf is installed, or install it
 which protoc > /dev/null 2>&1
@@ -19,9 +51,13 @@ if [[ $? -ne 0 ]]; then
   make -j4 > /dev/null 2>&1
   make check -j4 > /dev/null 2>&1
   make install > /dev/null 2>&1
-  echo "You will be asked for your password to set ldconfig."
+  echo "You might be asked for your password to set ldconfig."
   sudo ldconfig > /dev/null 2>&1
   
   cd .. 
   rm -rf protobuf-3.5.1
 fi
+
+# build kvs
+git clone https://github.com/cw75/tiered-storage
+cd tiered-storage && sh scripts/build_release.sh
