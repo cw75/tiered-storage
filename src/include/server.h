a9e06a543a7b6467d43cbf3fe1ca4cc399ad6991
Joe Hellerstein <jmh@berkeley.edu>
2018-07-03 17:52:12 -0700

    begin refactoring handlers from server

---
+++ src/include/server.h
@@ -0,0 +1,17 @@
+#include "utils/server_utility.h"
+void join_handler(unsigned int self_tier_id,
+             unsigned int thread_num,
+             string ip,
+             zmq::socket_t *join_puller_ref,
+             SocketCache& pushers,
+             unordered_map<unsigned, global_hash_t>& global_hash_ring_map,
+             unordered_map<unsigned, local_hash_t>& local_hash_ring_map,
+             unordered_map<string, key_info> placement,
+             std::shared_ptr<spdlog::logger> logger,
+             unsigned thread_id,
+             unsigned &seed,
+             unordered_map<string, key_stat>& key_stat_map,
+             server_thread_t& wt,
+             address_keyset_map join_addr_keyset_map,
+             unordered_set<string> join_remove_set
+             );
