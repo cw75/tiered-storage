833a20fcdb3f0d283f8ba366532748d284716f47
cw75 <cgwu0530@gmail.com>
2018-07-04 23:49:58 +0000

    revert build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,46 +1,11 @@
-#!/bin/bash
-args=( -j -t )
-containsElement () {
-  local e match="$1"
-  shift
-  for e; do [[ "$e" == "$match" ]] && return 0; done
-  return 1
-}
-
-while getopts ":j:t:" opt; do
-  case $opt in
-   j)
-     set MAKE_THREADS = $OPTARG
-     if containsElement $OPTARG "${args[@]}" 
-     then
-       echo "Missing argument to flag $opt"
-       exit 1
-     else
-       echo "make set to run on $OPTARG threads" >&2
-     fi
-     ;;
-   t)
-     set TYPE = $OPTARG
-     if containsElement $OPTARG "${args[@]}"
-     then
-       echo "Missing argument to flag $opt"
-       exit 1
-     else
-       echo "build type set to $OPTARG" >&2
-     fi
-     ;;
-   \?)
-     echo "Invalid option: -$OPTARG" >&2
-     exit 1
-     ;;
-  esac
-done
-
-if [[ -z "$MAKE_THREADS" ]]; then set MAKE_THREADS = 1; fi
-if [[ -z "$TYPE" ]]; then set TYPE = Release; fi
+if [[ -z $1 ]]; then
+  TYPE=Release
+else
+  TYPE=$1
+fi
 
 rm -rf build
 mkdir build
 cd build
-cmake -std=c++ll -DCMAKE_BUILD_TYPE=$TYPE ..
-make -j${MAKE_THREADS}
+cmake -std=c++11 -DCMAKE_BUILD_TYPE=$TYPE ..
+make
