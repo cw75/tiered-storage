42b243c59fb7339062af837573edf5fb976fee58
Joe Hellerstein <jmh@berkeley.edu>
2018-07-05 00:38:04 +0000

    reinstate server.cpp to a single file as in master

--- src/bedrock/server.cpp
+++ src/bedrock/server.cpp
@@ -19,7 +19,6 @@
 #include "zmq/zmq_util.h"
 #include "utils/consistent_hash_map.hpp"
 #include "common.h"
-#include "server.h"
 #include "hash_ring.h"
 #include "utils/server_utility.h"
 #include "yaml-cpp/yaml.h"
@@ -462,10 +461,72 @@ void run(unsigned thread_id) {
     // receives a node join
     if (pollitems[0].revents & ZMQ_POLLIN) {
       auto work_start = chrono::system_clock::now();
-      join_handler(SELF_TIER_ID, THREAD_NUM, ip, &join_puller, pushers, 
-                   global_hash_ring_map, local_hash_ring_map,
-                   placement, logger, thread_id, seed, key_stat_map, wt,
-                   join_addr_keyset_map, join_remove_set);
+      string message = zmq_util::recv_string(&join_puller);
+
+      vector<string> v;
+      split(message, ':', v);
+      unsigned tier = stoi(v[0]);
+      string new_server_ip = v[1];
+
+      // update global hash ring
+      bool inserted = insert_to_hash_ring<global_hash_t>(global_hash_ring_map[tier], new_server_ip, 0);
+
+      if (inserted) {
+        logger->info("Received a node join for tier {}. New node is {}", tier, new_server_ip);
+
+        // only relevant to thread 0
+        if (thread_id == 0) {
+          // gossip the new node address between server nodes to ensure consistency
+          for (auto it = global_hash_ring_map.begin(); it != global_hash_ring_map.end(); it++) {
+            unsigned tier_id = it->first;
+            auto hash_ring = &(it->second);
+            unordered_set<string> observed_ip;
+            for (auto iter = hash_ring->begin(); iter != hash_ring->end(); iter++) {
+              if (iter->second.get_ip().compare(ip) != 0 && iter->second.get_ip().compare(new_server_ip) != 0 && observed_ip.find(iter->second.get_ip()) == observed_ip.end()) {
+                // if the node is not myself and not the newly joined node, send the ip of the newly joined node
+                zmq_util::send_string(message, &pushers[(iter->second).get_node_join_connect_addr()]);
+                observed_ip.insert(iter->second.get_ip());
+              } else if (iter->second.get_ip().compare(new_server_ip) == 0 && observed_ip.find(iter->second.get_ip()) == observed_ip.end()) {
+                // if the node is the newly joined node, send my ip
+                zmq_util::send_string(to_string(SELF_TIER_ID) + ":" + ip, &pushers[(iter->second).get_node_join_connect_addr()]);
+                observed_ip.insert(iter->second.get_ip());
+              }
+            }
+          }
+
+          // tell all worker threads about the new node join
+          for (unsigned tid = 1; tid < THREAD_NUM; tid++) {
+            zmq_util::send_string(message, &pushers[server_thread_t(ip, tid).get_node_join_connect_addr()]);
+          }
+
+          for (auto it = global_hash_ring_map.begin(); it != global_hash_ring_map.end(); it++) {
+            logger->info("Hash ring for tier {} is size {}.", to_string(it->first), to_string(it->second.size()));
+          }
+        }
+
+        if (tier == SELF_TIER_ID) {
+          vector<unsigned> tier_ids;
+          tier_ids.push_back(SELF_TIER_ID);
+          bool succeed;
+
+          for (auto it = key_stat_map.begin(); it != key_stat_map.end(); it++) {
+            string key = it->first;
+            auto threads = get_responsible_threads(wt.get_replication_factor_connect_addr(), key, is_metadata(key), global_hash_ring_map, local_hash_ring_map, placement, pushers, tier_ids, succeed, seed);
+
+            if (succeed) {
+              if (threads.find(wt) == threads.end()) {
+                join_remove_set.insert(key);
+
+                for (auto iter = threads.begin(); iter != threads.end(); iter++) {
+                  join_addr_keyset_map[iter->get_gossip_connect_addr()].insert(key);
+                }
+              }
+            } else {
+              logger->info("Error: missing key replication factor in node join routine.");
+            }
+          }
+        }
+      }
       auto time_elapsed = chrono::duration_cast<chrono::microseconds>(chrono::system_clock::now()-work_start).count();
       working_time += time_elapsed;
       working_time_map[0] += time_elapsed;
