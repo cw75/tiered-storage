e248ee4ca6b3e4e174d13a0509d129ccbbe93d0d
Vikram Sreekanti <vsreekanti@gmail.com>
2018-07-13 14:36:16 -0700

    Adds checks to travis build

--- scripts/check-clang.sh
+++ scripts/check-clang.sh
@@ -1,39 +1,31 @@
 #  Copyright 2018 U.C. Berkeley RISE Lab
-# 
+#
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
 #  You may obtain a copy of the License at
-# 
+#
 #      http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 #  Unless required by applicable law or agreed to in writing, software
 #  distributed under the License is distributed on an "AS IS" BASIS,
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-language: cpp
-sudo: required
+#!/bin/bash
 
-os: 
-  - linux
-dist: trusty
+cd build && make clang-format && cd ..
 
-compiler:
-  - clang
+# see if any files were modified by clang-format
+FILES=`git status | grep modified`
+MODIFIED=`$FILES | wc -l`
 
-# NOTE: It might be bad to depend on $HOME?
-env:
-  - PROTOBUF_DIR="$HOME/protobuf"
+if [[ $MODIFIED -gt 0 ]]; then
+  echo "clang-format check failed. The following files had style issues:\n"
+  echo $FILES
 
-cache:
-  directories:
-    - $PROTOBUF_DIR
-
-install:
-  - ./scripts/travis-install.sh
-
-script:
-  - ./scripts/check-clang.sh
-  - ./scripts/build.sh
-  - ./tests/simple/test-simple.sh
+  exit 1
+else
+  echo "clang-format check succeeded!"
+  exit 0
+fi
--- scripts/travis-install.sh
+++ scripts/travis-install.sh
@@ -14,6 +14,25 @@
 
 #!/bin/bash
 
+download_protobuf() {
+  rm -rf $PROTOBUF_DIR
+
+  wget https://github.com/google/protobuf/releases/download/v3.5.1/protobuf-all-3.5.1.zip
+  unzip protobuf-all-3.5.1.zip > /dev/null 2>&1
+  mv protobuf-3.5.1 $PROTOBUF_DIR
+  rm protobuf-all-3.5.1.zip
+
+  cd $PROTOBUF_DIR && ./autogen.sh
+  cd $PROTOBUF_DIR && ./configure CXX=clang++ CXXFLAGS='-std=c++11 -stdlib=libc++ -O3 -g'
+}
+
+install_protobuf() {
+  cd $PROTOBUF_DIR && make -j4
+
+  cd $PROTOBUF_DIR && sudo make install
+  cd $PROTOBUF_DIR && sudo ldconfig
+}
+
 sudo apt-get update
 sudo apt-get install -y build-essential autoconf automake libtool curl make unzip pkg-config wget
 sudo apt-get install -y libc++-dev libc++abi-dev awscli jq
@@ -21,14 +40,9 @@ sudo apt-get install -y libc++-dev libc++abi-dev awscli jq
 sudo ln -s $(which clang) /usr/bin/clang
 sudo ln -s $(which clang++) /usr/bin/clang++
 
-wget https://github.com/google/protobuf/releases/download/v3.5.1/protobuf-all-3.5.1.zip
-unzip protobuf-all-3.5.1.zip > /dev/null 2>&1
-rm protobuf-all-3.5.1.zip
+# if the protobuf directory doesn't exist
+if [ ! -d "$PROTOBUF_DIR" ]; then
+  download_protobuf
+fi
 
-cd protobuf-3.5.1
-./autogen.sh
-./configure CXX=clang++ CXXFLAGS='-std=c++11 -stdlib=libc++ -O3 -g'
-make -j4
-sudo make install
-sudo ldconfig
-cd ..
+install_protobuf
